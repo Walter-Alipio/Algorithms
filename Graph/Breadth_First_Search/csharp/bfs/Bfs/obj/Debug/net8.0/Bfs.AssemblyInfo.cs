a416657d36510410408eacd0c229a22d1b2ace14
walter alipio <silvawalteralipio@gmail.com>
2024-09-01 17:32:01 -0300

    update version

--- Graph/Breadth_First_Search/csharp/bfs/Bfs/obj/Debug/net8.0/Bfs.AssemblyInfo.cs
+++ Graph/Breadth_First_Search/csharp/bfs/Bfs/obj/Debug/net8.0/Bfs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bfs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd696a4fe10aeb8ab0157a8930bcc960369ba718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4fa08743875152903577615d31214146776a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bfs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bfs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
